b5786d69a1b09b5e6a6b929b3e2e4af0a41cb05d
Tushar <tusharimran470@gmail.com>
2024-06-15 15:53:57 +0600

    delete data from table added

--- src/Components/Dashboard/AddProduct.jsx
+++ src/Components/Dashboard/AddProduct.jsx
@@ -7,120 +7,120 @@ import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const AddProduct = () => {
     const [category, setCategory] = useState('apple');
-    const axiosSecure=useAxiosSecure()
+    const axiosSecure = useAxiosSecure()
     const handleSelectChange = (event) => {
-      setCategory(event.target.value);
+        setCategory(event.target.value);
     };
 
 
-    const { register, handleSubmit,reset, formState: { errors }, } = useForm()
+    const { register, handleSubmit, reset, formState: { errors }, } = useForm()
 
-    const onSubmit = async(data) => {
-        
+    const onSubmit = async (data) => {
+
         try {
             const imageFile = data.image[0];
             const imageUrl = await imageUpload(imageFile);
             const image = imageUrl?.data?.display_url;
-      
+
             const product = { ...data, Category: category, imageUrl: image };
-      
+
 
             //    console.log(product)
 
             const res = await axiosSecure.post('/products', product);
             if (res?.data?.insertedId) {
-              toast.success("Your mobile is succesfully uploaded");
-              reset();
+                toast.success("Your mobile is succesfully uploaded");
+                reset();
             }
-          } catch (error) {
+        } catch (error) {
             console.error('Error upload img', error);
-          }
-        };
-      
-    
-    
-
-  return (
-    <div>
+        }
+    };
 
 
-             <h1 className="font-bold text-2xl text-center py-5 border-b-2 w-[20%] mx-auto">Upload Mobile</h1>
 
 
-         <form className="mt-5" onSubmit={handleSubmit(onSubmit)}>
-        
-        <div className="lg:flex gap-4">
-        <div className="form-control w-[50%]">
-            <label className="label">
-              <span className="label-text">Name</span>
-            </label>
-            <input   {...register("Name")} type="text" placeholder="Name" className="input input-bordered"  required />
-          </div>
-    
-        <div className="form-control  w-[50%]">
-            <label className="label">
-              <span className="label-text">Storage</span>
-            </label>
-            <input  {...register("Storage")} type="text" placeholder="Storage" className="input input-bordered" required />
-          </div>
-        </div>
-    
-    
-        <div className="lg:flex gap-4">
-        <div className="form-control w-[50%]">
-            <label className="label">
-              <span className="label-text">Ram</span>
-            </label>
-            <input  {...register("Ram")} type="text" placeholder="Ram" className="input input-bordered"  required />
-          </div>
-    
-        <div className="form-control  w-[50%]">
-            <label className="label">
-              <span className="label-text">Price</span>
-            </label>
-            <input  {...register("price")} type="text" placeholder="Price" className="input input-bordered"  required />
-          </div>
-        </div>
-    
-    
-        <div className="lg:flex gap-4">
-        <div className="form-control w-[50%]">
-            <label className="label">
-              <span className="label-text">Bettery</span>
-            </label>
-            <input  {...register("Bettery")} type="text" placeholder="Bettery" className="input input-bordered"  required />
-          </div>
-    
-        <div className="form-control  w-[50%]">
-            <label className="label">
-              <span className="label-text">Display</span>
-            </label>
-            <input   {...register("Display")} type="text" placeholder="Display" className="input input-bordered" required />
-          </div>
-        </div>
-    
-    
-      
-    
-        
-        <div className="lg:flex gap-4">
-        <div className="form-control w-[50%]">
-            <label className="label">
-              <span className="label-text">Camara</span>
-            </label>
-            <input   defaultValue='' {...register("Camara")} type="text" placeholder="Camara"  className="input input-bordered" required />
-          </div>
-    
-        <div className="form-control  w-[50%]">
-            <label className="label">
-              <span className="label-text">Status</span>
-            </label>
-            <input defaultValue=''  {...register("Status")} type="text" placeholder="Status" className="input input-bordered" required />
-          </div>
-        </div>
-    
-    
-        {/* <div className="lg:flex gap-4">
+    return (
+        <div>
+
+
+            <h1 className="font-bold text-2xl text-center py-5 border-b-2 w-[20%] mx-auto">Upload Mobile</h1>
+
+
+            <form className="mt-5" onSubmit={handleSubmit(onSubmit)}>
+
+                <div className="lg:flex gap-4">
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input   {...register("Name")} type="text" placeholder="Name" className="input input-bordered" required />
+                    </div>
+
+                    <div className="form-control  w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Storage</span>
+                        </label>
+                        <input  {...register("Storage")} type="text" placeholder="Storage" className="input input-bordered" required />
+                    </div>
+                </div>
+
+
+                <div className="lg:flex gap-4">
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Ram</span>
+                        </label>
+                        <input  {...register("Ram")} type="text" placeholder="Ram" className="input input-bordered" required />
+                    </div>
+
+                    <div className="form-control  w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <input  {...register("price")} type="text" placeholder="Price" className="input input-bordered" required />
+                    </div>
+                </div>
+
+
+                <div className="lg:flex gap-4">
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Bettery</span>
+                        </label>
+                        <input  {...register("Bettery")} type="text" placeholder="Bettery" className="input input-bordered" required />
+                    </div>
+
+                    <div className="form-control  w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Display</span>
+                        </label>
+                        <input   {...register("Display")} type="text" placeholder="Display" className="input input-bordered" required />
+                    </div>
+                </div>
+
+
+
+
+
+                <div className="lg:flex gap-4">
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Camara</span>
+                        </label>
+                        <input defaultValue='' {...register("Camara")} type="text" placeholder="Camara" className="input input-bordered" required />
+                    </div>
+
+                    <div className="form-control  w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Status</span>
+                        </label>
+                        <input defaultValue=''  {...register("Status")} type="text" placeholder="Status" className="input input-bordered" required />
+                    </div>
+                </div>
+
+
+                {/* <div className="lg:flex gap-4">
         <div className="form-control w-[50%]">
             <label className="label">
               <span  className="label-text">Admin Name</span>
@@ -135,46 +135,46 @@ const AddProduct = () => {
             <input  defaultValue={data.email}  {...register("admin_email")} type="email" placeholder="Distributor Email" className="input input-bordered" required />
           </div>
         </div> */}
-    
-    
-        <div className="lg:flex gap-4">
-        <div className="form-control w-[50%]">
-            <label className="label">
-              <span className="label-text">Image</span>
-            </label>
-            <input {...register("image")} type="file" placeholder=""   accept='image/*' id="image" className="input input-bordered" required />
-          </div>
-    
-        <div className="form-control  w-[50%]">
-            
-            <label htmlFor="selectOption">Brand</label>
-        <select className="input input-bordered" id="category" name="category" value={category} onChange={handleSelectChange}>
-          <option value="samsung">samsung</option>
-          <option value="apple">apple</option>
-          <option value="vivo">vivo</option>
-          <option value="oppo">oppo</option>
-          <option value="realme">realme</option>
-          <option value="redme">redme</option>
-          <option value="howay">howay</option>
-          <option value="infinix">infinix</option>
-      
-        </select>
-          </div>
-        </div>
-    
-    
-            <div className="form-control mt-2 w-[100%]">
-              <input
-                type="submit"
-                value="Upload Mobile"
-                className="input input-bordered bg-[#0000004d] cursor-pointer text-white"
-              />
-            </div>
-            {errors.exampleRequired && <span>This field is required</span>}
-          </form>
 
-    </div>
-  )
+
+                <div className="lg:flex gap-4">
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Image</span>
+                        </label>
+                        <input {...register("image")} type="file" placeholder="" accept='image/*' id="image" className="input input-bordered" required />
+                    </div>
+
+                    <div className="form-control  w-[50%]">
+
+                        <label htmlFor="selectOption">Brand</label>
+                        <select className="input input-bordered" id="category" name="category" value={category} onChange={handleSelectChange}>
+                            <option value="samsung">samsung</option>
+                            <option value="apple">apple</option>
+                            <option value="vivo">vivo</option>
+                            <option value="oppo">oppo</option>
+                            <option value="realme">realme</option>
+                            <option value="redme">redme</option>
+                            <option value="howay">howay</option>
+                            <option value="infinix">infinix</option>
+
+                        </select>
+                    </div>
+                </div>
+
+
+                <div className="form-control mt-2 w-[100%]">
+                    <input
+                        type="submit"
+                        value="Upload Mobile"
+                        className="input input-bordered bg-[#0000004d] cursor-pointer text-white"
+                    />
+                </div>
+                {errors.exampleRequired && <span>This field is required</span>}
+            </form>
+
+        </div>
+    )
 }
 
 export default AddProduct
--- src/Components/Dashboard/AllProducts.jsx
+++ src/Components/Dashboard/AllProducts.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { MdOutlineUpdate } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 
 
@@ -9,82 +10,103 @@ import { MdDelete } from "react-icons/md";
 
 const AllProducts = () => {
 
-    const axiosSecure=useAxiosSecure()
+    const axiosSecure = useAxiosSecure()
 
-    const { data: products = [] } = useQuery({
+    const { data: products = [], refetch } = useQuery({
         queryKey: ['products'],
         queryFn: async () => {
-          const res = await axiosSecure.get('/products');
-          return res.data;
+            const res = await axiosSecure.get('/products');
+            return res.data;
         },
-      });
+    });
 
 
-      console.log(products)
+    //   console.log(products)
+
+    const handleDelete = (id) => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // console.log(id)
+                axiosSecure.delete(`products/${id}`)
+                    .then((res) => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            })
+                            refetch()
+
+                        }
+                    })
+            }
+        })
+    }
 
 
-  return (
-    <div>
-  <h1 className="font-bold text-2xl text-center py-5 border-b-2 w-[20%] mx-auto">Uploaded Mobiles</h1>
+    return (
+        <div>
+            <h1 className="font-bold text-2xl text-center py-5 border-b-2 w-[20%] mx-auto">Uploaded Mobiles</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            #
+                            <th>Model</th>
+                            <th>Brand</th>
+                            <th>Ram</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {products.map((product, idx) => {
+                            //   {   console.log(product)}
+
+                            return <tr key={product._id}>
+                                <th>
+                                    {idx + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+
+                                        <div>
+                                            <div className="font-bold">{product?.Name}</div>
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {product?.Category}
+                                </td>
+                                <td>{product?.Ram}</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs"><span className="text-xl"><MdOutlineUpdate /></span></button>
+                                </th>
+                                <th>
+                                    <button onClick={() => handleDelete(product?._id)} className="btn btn-ghost btn-xs"><span className="text-xl"><MdDelete /></span></button>
+                                </th>
+                            </tr>
 
 
+                        })}
 
-  <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        #
-        <th>Model</th>
-        <th>Brand</th>
-        <th>Ram</th>
-        <th>Update</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
- 
-          {products.map((product,idx)=>{
-  {   console.log(product)}
-
-return <tr key={product._id}>
-<th>
-  {idx+1}
-</th>
-<td>
-  <div className="flex items-center gap-3">
-   
-    <div>
-      <div className="font-bold">{product?.Name}</div>
-     
-    </div>
-  </div>
-</td>
-<td>
-{product?.Category}
-</td>
-<td>{product?.Ram}</td>
-<th>
-  <button className="btn btn-ghost btn-xs"><span className="text-xl"><MdOutlineUpdate/></span></button>
-</th>
-<th>
-  <button className="btn btn-ghost btn-xs"><span className="text-xl"><MdDelete/></span></button>
-</th>
-</tr>
-
-
-          })}
-      
-    </tbody> 
-  </table>
-</div>
-
-  
-
-     
-
-    </div>
-  )
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default AllProducts
