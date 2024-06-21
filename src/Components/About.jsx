3aa4505f9182de6476edb45cd5c47e9f11c5c2f2
Tushar <tusharimran470@gmail.com>
2024-06-21 11:37:41 +0600

    why choose us section is added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import aboutbg from '../assets/About-bg.jpg'
 import aboutBrand from '../assets/aboutBrand.jpg'
+import whyUs from '../assets/whyus.jpg'
 
 const About = () => {
   return (
@@ -33,6 +34,40 @@ const About = () => {
             </div>
         </div>
 
+
+        {/* whyChooseUs */}
+
+        <div>
+           <h1 className="text-center font-bold text-2xl mt-10">Why choose us</h1>
+           <p className="text-center text-gray-300 mt-5">Aenean non erat vitae tortor aliquet varius. Vivamus aliquam volutpat semper. Integer dignissim vitae arcu <br /> imperdiet imperdiet. Nullam varius sapien sed enim consequat, eleifend velit ullamcorper.
+           </p>
+
+            <div className='flex w-[80%] mx-auto mt-10 items-center justify-center'>
+                        <div className='w-[50%]'>
+                          <img src={whyUs} alt="" />
+                        </div>
+
+
+                       <div className='w-[50%]'>
+                                <div className='mb-5'>
+                                    <h1 className='font-bold text-gray-300 text-xl'>01</h1>
+                                    <h1 className="text-lg font-semibold">Minimal design</h1>
+                                    <p className="text-gray-300">Fusce mollis urna tortorInteger dignissim vitae arcu imperdiet imperdiet. <br />  Nullam varius sapien sed enim consequat, eleifend velit ullamcorper.</p>
+                                </div>
+                                <div className='mb-5'>
+                                    <h1 className='font-bold text-gray-300 text-xl'>02</h1>
+                                    <h1 className="text-lg font-semibold">Minimal design</h1>
+                                    <p className="text-gray-300">Fusce mollis urna tortorInteger dignissim vitae arcu imperdiet imperdiet. <br />  Nullam varius sapien sed enim consequat, eleifend velit ullamcorper.</p>
+                                </div>
+                                <div className='mb-5'>
+                                    <h1 className='font-bold text-gray-300 text-xl'>03</h1>
+                                    <h1 className="text-lg font-semibold">Minimal design</h1>
+                                    <p className="text-gray-300">Fusce mollis urna tortorInteger dignissim vitae arcu imperdiet imperdiet. <br />  Nullam varius sapien sed enim consequat, eleifend velit ullamcorper.</p>
+                                </div>
+                       </div>
+            </div>
+
+        </div>
 
     </div>
   )
